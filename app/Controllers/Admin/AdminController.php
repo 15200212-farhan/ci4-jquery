1d8a0563ddfd16f02c6a0dc50f086961af015686
15200212-farhan <farhan18apr02@gmail.com>
2022-03-17 11:24:10 +0700

    penambahan controller read,hapus, edit

--- app/Controllers/Admin/AdminController.php
+++ app/Controllers/Admin/AdminController.php
@@ -4,7 +4,6 @@ namespace App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\MahasiswaModel;
-use CodeIgniter\Exceptions\PageNotFoundException;
 
 class AdminController extends BaseController
 {
@@ -30,72 +29,93 @@ class AdminController extends BaseController
         $data['title'] = 'Admin - list-data';
         $data['mahasiswa'] = $this->mahasiswa_model->get_mahasiswa();
 
+
         return view('admin/list-data', $data);
     }
 
-    public function form_tambahdata()
+    public function modal_tambah()
     {
-
-        if (!$this->request->isAJAX()) {
-            throw PageNotFoundException::forPageNotFound();
-            return false;
-        }
-
-        $response = [
-            'data' => view('admin/ajax/form-tambahdata')
-        ];
-
-        echo json_encode($response);
-    }
-
-
-    public function insert_datamahasiswa()
-    {
-        if (!$this->request->isAJAX()) {
-            throw PageNotFoundException::forPageNotFound();
-            return false;
-        }
-        $validation = \Config\Services::validation();
-        $valid = $this->validate([
-            'v_nim' => [
-                'rules' => 'required|is_unique[tbl_mahasiswa.nim]',
-                'label' => 'nim',
-                'errors' => [
-                    'required' => '{field} belum disi!',
-                    'is_unique' => '{field} sudah tersedia.'
-                ]
-            ],
-            'v_nama' => [
-                'rules' => 'required',
-                'label' => 'nama',
-                'errors' => [
-                    'required' => '{field} belum disi!',
-                ]
-            ]
-        ]);
-
-        if (!$valid) {
+        if ($this->request->isAJAX()) {
             $msg = [
-                'error' => [
-                    'v_nim' => $validation->getError('v_nim'),
-                    'v_nama' => $validation->getError('v_nama'),
-                ]
+                'data' => view('admin/ajax/modal-tambah')
             ];
-        } else {
-            $data_mhs = [
-                'nim' => $this->request->getPost('v_nim'),
-                'nama' => $this->request->getPost('v_nama'),
-                'prodi' => $this->request->getPost('v_prodi'),
-                'email' => $this->request->getPost('v_email'),
-            ];
-            $this->mahasiswa_model->insert_datamahasiswa($data_mhs);
-            $msg = ['success' => 'Berhasil menambahkan data baru...'];
-        }
 
-        echo json_encode($msg);
+            echo json_encode($msg);
+        }
     }
 
-    public function form_editdata()
+    public function simpan_data()
     {
+        if ($this->request->isAJAX()) {
+            $validation = \Config\Services::validation();
+            $valid = $this->validate([
+                'v_nim' => [
+                    'rules' => 'required|is_unique[tbl_mahasiswa.nim]',
+                    'errors' => [
+                        'required' => 'nim wajib diisi!',
+                        'is_unique' => 'nim sudah terdaftar'
+                    ]
+                ]
+            ]);
+            if (!$valid) {
+                $msg = [
+                    'error' => [
+                        'v_nim' => $validation->getError('v_nim')
+                    ]
+                ];
+            } else {
+                $data = [
+                    'nim'   => $this->request->getPost('v_nim'),
+                    'nama'  => $this->request->getPost('v_nama'),
+                    'prodi' => $this->request->getPost('v_prodi'),
+                    'email' => $this->request->getPost('v_email'),
+                ];
+                $this->mahasiswa_model->insert_datamahasiswa($data);
+                $msg = ['success' => 'berhasil menambahkan data baru.'];
+            }
+
+            echo json_encode($msg);
+        }
+    }
+
+    public function modal_edit()
+    {
+        if ($this->request->isAJAX()) {
+            $id = $this->request->getVar('id');
+            $row = $this->mahasiswa_model->get_mahasiswa($id);
+            $send_data = [
+                'id'   => $row['nim'],
+                'nama'  => $row['nama'],
+                'prodi' => $row['prodi'],
+                'email' => $row['email']
+            ];
+            $msg = ['success' => view('admin/ajax/modal-edit', $send_data)];
+            echo json_encode($msg);
+        }
+    }
+
+    public function ubah_data()
+    {
+        if ($this->request->isAJAX()) {
+            $id = $this->request->getPost('v_nim'); // PK
+            $data = [
+                'nama'  => $this->request->getPost('v_nama'),
+                'prodi' => $this->request->getPost('v_prodi'),
+                'email' => $this->request->getPost('v_email')
+            ];
+            $this->mahasiswa_model->update($id, $data);
+            $msg = ['success' => $id . '' . ' berhasil diperbarui'];
+            echo json_encode($msg);
+        }
+    }
+
+    public function hapus_data()
+    {
+        if ($this->request->isAJAX()) {
+            $id = $this->request->getVar('id');
+            $this->mahasiswa_model->delete($id);
+            $msg = ['success' => $id . '' . ' berhasil dihapus'];
+            echo json_encode($msg);
+        }
     }
 }
